0142fbaed3043c565c988f2e2c5a707b6489a661
leo-creato <leo.bouchet@cpe.fr>
2020-11-02 16:21:11 +0100

    css

--- js/destination.js
+++ js/destination.js
@@ -20,4 +20,17 @@ var Damas = new Destinations("Damas","../","100")
     let NbrAdultes= document.getElementsByName("Nombre d'adulte(s)")
     let NbrEnfants= document.getElementsByName("Nombre d'enfant(s)")
 
-    
+    //Afficher le Prix total
+
+    document.getElementById("bPrix").addEventListener("click", afficherprix(100,2,1,1,1,4));
+    function afficherprix(pPrix,pAdultes,pEnfants,pAnimaux,pPtitDej,pNuit) {
+        console.log("Toto")
+        let PrixTotal=(pPrix*pAdultes +(4/10)*pPrix*pEnfants)*pNuit
+        if (pAnimaux==1){
+            PrixTotal+=10*pNuit
+        }
+        if (pPtitDej==1){
+            PrixTotal+=12*(pAdultes +(4/10)*pEnfants)
+        }
+    }
+        document.getElementById("zoneprix").innerHTML = PrixTotal + "Euros"
