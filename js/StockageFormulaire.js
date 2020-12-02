8ac04c977c00bb173a8fad8948c59cad857206a9
leo-creato <leo.bouchet@cpe.fr>
2020-12-02 17:55:52 +0100

    stockage des données terminer, début de création d'une pâge pour le compte client

--- js/StockageFormulaire.js
+++ js/StockageFormulaire.js
@@ -6,7 +6,7 @@ console.log(btnEnregistrermesinformations);
 btnEnregistrermesinformations.addEventListener("click", () => {
     //récupérer les données de mon formulaire
     let ptitdej = document.querySelectorAll("input[name = 'Ptit_dej']"); // pour le ptit dej (on doit cocher aussi)
-
+    
     for (i = 0; i< ptitdej.length; i++) {
         if (ptitdej[i].checked === true){
         PtitDej = ptitdej[i].value;
@@ -21,23 +21,23 @@ btnEnregistrermesinformations.addEventListener("click", () => {
     localStorage.setItem("adresse", document.querySelector("#adresse").value);
     localStorage.setItem("date_depart", document.querySelector("#date_depart").value);
     localStorage.setItem("date_arrivee", document.querySelector("#date_arrivee").value);
-    localStorage.setItem("Ptit_dej", Ptitdej)
     localStorage.setItem("adulte", document.querySelector("#adulte").value);
     localStorage.setItem("enfant", document.querySelector("#enfant").value);
     localStorage.setItem("animal", document.querySelector("#animaux").value);
+    localStorage.setItem("Ptit_dej", PtitDej)
 
 
     //Récuperer les données et les afficher
 
-    document.querySelector("#LeNom").innerHTML = localStorage.getItem("lastname")
-    document.querySelector("#LePrenom").innerHTML = localStorage.getItem("firstname")
-    document.querySelector("#AdresseMail").innerHTML = localStorage.getItem("adresse_mail")
-    document.querySelector("#NumTelephone").innerHTML = localStorage.getItem("telephone")
-    document.querySelector("#adress").innerHTML = localStorage.getItem("adresse")
-    document.querySelector("#DateDepart").innerHTML = localStorage.getItem("date_depart")
-    document.querySelector("#DateRetour").innerHTML = localStorage.getItem("date_arrivee")
-    document.querySelector("#Ptit_Dej").innerHTML = localStorage.getItem("Ptit_dej")
-    document.querySelector("#NbrAdulte").innerHTML = localStorage.getItem("adulte")
-    document.querySelector("#NbrEnfant").innerHTML = localStorage.getItem("enfant")
-    document.querySelector("#Animal").innerHTML = localStorage.getItem("animal")
+    document.querySelector("#LeNom").innerHTML = localStorage.getItem("lastname");
+    document.querySelector("#LePrenom").innerHTML = localStorage.getItem("firstname");
+    document.querySelector("#AdresseMail").innerHTML = localStorage.getItem("adresse_mail");
+    document.querySelector("#NumTelephone").innerHTML = localStorage.getItem("telephone");
+    document.querySelector("#Adresse").innerHTML = localStorage.getItem("adresse");
+    document.querySelector("#DateDepart").innerHTML = localStorage.getItem("date_depart");
+    document.querySelector("#DateRetour").innerHTML = localStorage.getItem("date_arrivee");
+    document.querySelector("#NbrAdulte").innerHTML = localStorage.getItem("adulte");
+    document.querySelector("#NbrEnfant").innerHTML = localStorage.getItem("enfant");
+    document.querySelector("#Animal").innerHTML = localStorage.getItem("animal");
+    document.querySelector("#Ptit_Dej").innerHTML = localStorage.getItem("Ptit_dej");
 })
