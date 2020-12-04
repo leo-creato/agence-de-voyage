83aa86c4c77c539829aefac083ab7c8a27d07e69
leo-creato <leo.bouchet@cpe.fr>
2020-12-04 13:30:37 +0100

    recuperation des données sur la page récapitulative

---
+++ recuperationformulaire.js
@@ -0,0 +1,11 @@
+document.querySelector("#LeNom").innerHTML = localStorage.getItem("lastname");
+document.querySelector("#LePrenom").innerHTML = localStorage.getItem("firstname");
+document.querySelector("#AdresseMail").innerHTML = localStorage.getItem("adresse_mail");
+document.querySelector("#NumTelephone").innerHTML = localStorage.getItem("telephone");
+document.querySelector("#Adresse").innerHTML = localStorage.getItem("adresse");
+document.querySelector("#DateDepart").innerHTML = localStorage.getItem("date_depart");
+document.querySelector("#DateRetour").innerHTML = localStorage.getItem("date_arrivee");
+document.querySelector("#NbrAdulte").innerHTML = localStorage.getItem("adulte");
+document.querySelector("#NbrEnfant").innerHTML = localStorage.getItem("enfant");
+document.querySelector("#Animal").innerHTML = localStorage.getItem("animal");
+document.querySelector("#Ptit_Dej").innerHTML = localStorage.getItem("Ptit_dej");
